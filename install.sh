f0d709efe639268761f2433cbafd69a90cfdf6d4
Lance Nickel <gorp@lanickel.com>
2023-05-04 02:35:30 -0400

    update installer

--- install.sh
+++ install.sh
@@ -129,12 +129,6 @@ sudo cp gorp.conf /usr/local/etc/
 
 #### GET LATEST JAR FILE ############
 
-#### Get newly-installed params
-
-. /usr/local/bin/gorpmc/functions/params.sh || echo "Installation error -- Failed to get new config paremeters. Please uninstall Gorp and try again. If this issue persists, open an issue." && exit 1
-
-
-
 #### Set build to 000 to force a JAR download
 
 echo "paper-0-000.jar" > $HOMEDIR/gorpmc/jars/latest
